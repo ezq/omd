59303e4aba6ff5c68689600a553ec72c93e26746
ezq <ecardinali@gmail.com>
2020-10-09 08:28:58 -0300

    force apt install

--- scripts/install_common.sh
+++ scripts/install_common.sh
@@ -9,7 +9,7 @@ echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.d/20-ipv6-disable.conf
 cat /etc/sysctl.d/20-ipv6-disable.conf; sysctl -p
 
 apt-get update
-apt-get install -y lsof vim git openssh-server tree tcpdump libevent-2.0-5 file make sudo lsyncd screen wget python
+apt-get install -fy lsof vim git openssh-server tree tcpdump libevent-2.0-5 file make sudo lsyncd screen wget python 
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
